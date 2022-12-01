d237894061c4946079738460d6dc278960a7c3f2
Nadine Zbinden <nadine.zbinden@students.fhnw.ch>
2022-12-01 12:37:03 +0100

    add cSharp solutions solution and solve first day stars

---
+++ CSharpSolutions/ConsoleAppSolutions/Day1/CalorieCounting.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleAppSolutions.Day1
+{
+    internal class CalorieCounting
+    {
+        static readonly string exampleInput = @"C:\Code\for fun\AdventOfCode\CSharpSolutions\ConsoleAppSolutions\Day1\Star1\InputExample.txt";
+        static readonly string actualInput = @"C:\Code\for fun\AdventOfCode\CSharpSolutions\ConsoleAppSolutions\Day1\Star1\InputActual.txt";
+
+        public static void GetElfWithMostCalories(bool useExampleInput)
+        {
+            var textFile = useExampleInput ? exampleInput : actualInput;
+
+            if (File.Exists(textFile))
+            {
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    int elfCounter = 1;
+                    string ln;
+
+                    var elfNumberWithItems = new Dictionary<int, List<int>>();
+                    elfNumberWithItems.Add(elfCounter, new List<int>());
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        if (string.IsNullOrEmpty(ln))
+                        {
+                            Console.WriteLine(elfNumberWithItems[elfCounter].Sum());
+
+                            elfCounter++;
+                            elfNumberWithItems.Add(elfCounter, new List<int>());
+                        }
+                        else
+                        {
+                            elfNumberWithItems[elfCounter].Add(int.Parse(ln));
+                        }
+                    }
+
+                    file.Close();
+                    Console.WriteLine($"File has {elfCounter} elves.");
+
+                    foreach (var elf in elfNumberWithItems.Select(e => e.Value.Sum()).OrderBy(e => e))
+                    {
+                        Console.WriteLine(elf);
+                    }
+
+                    var result = elfNumberWithItems.MaxBy(i => i.Value.Sum());
+                    Console.WriteLine($"Elf {result.Key} has the most calories: {result.Value.Sum()}");
+
+                    var resultOfTopThree = elfNumberWithItems.Select(e => e.Value.Sum()).OrderByDescending(e => e).Take(3);
+                    Console.WriteLine($"Top three elves have in total calories: {resultOfTopThree.Sum()}");
+                }
+            }
+        }
+    }
+}
---
+++ CSharpSolutions/ConsoleAppSolutions/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleAppSolutions.Day1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        CalorieCounting.GetElfWithMostCalories(useExampleInput: true);
+        CalorieCounting.GetElfWithMostCalories(useExampleInput: false);
+    }
+}
